432b941bde3d9d8ecdc869543da937d3b102fa37
Stephen Herbein <stephen272@gmail.com>
2020-01-31 13:59:05 -0800

    initial implementation to get unit tests passing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,132 @@
-use std::collections::HashMap;
+use std::iter::FromIterator;
+use std::collections::{HashMap,HashSet};
+
+struct Job {
+    jobid: i64,
+    ancestors: HashSet<i64>, // jobs this job depends on
+    children: HashSet<i64>, // jobs that depend on this job
+}
+
+impl Job {
+    pub fn new(jobid: i64) -> Self {
+        Self {
+            jobid: jobid,
+            ancestors: HashSet::new(),
+            children: HashSet::new(),
+        }
+    }
+}
 
 struct State {
-    deps: HashMap<i64, i64>,
+    jobs: HashMap<i64, Job>,
+    jobs_with_out_symbol: HashMap<String, HashSet<i64>>,
 }
 
 impl State {
     pub fn new() -> Self {
         Self {
-            deps: HashMap::new(),
+            jobs: HashMap::new(),
+            jobs_with_out_symbol: HashMap::new(),
         }
+    }
+
+    pub fn add_in_dependency(&mut self, in_job: &mut Job, symbol: &String) -> Result<(), &'static str> {
+        match self.jobs_with_out_symbol.get(symbol) {
+            Some(out_jobs) => {
+                for out_jobid in out_jobs.iter() {
+                    let out_job: &mut Job = match self.jobs.get_mut(out_jobid) {
+                        Some(x) => x,
+                        None => return Err("Matching out jobid found in symbol map but not job map")
+                    };
+                    in_job.ancestors.insert(out_job.jobid);
+                    out_job.children.insert(in_job.jobid);
+                };
+                Ok(())
+            },
+            None => {Ok(())},
+        }
+    }
+
+    pub fn add_out_dependency(&mut self, out_job: &Job, symbol: &String) {
+        match self.jobs_with_out_symbol.get_mut(symbol) {
+            Some(out_jobs) => {
+                out_jobs.insert(out_job.jobid);
+            },
+            None => {
+                self.jobs_with_out_symbol.insert(symbol.clone(), vec![out_job.jobid].into_iter().collect());
+            },
+        };
+    }
+
+    pub fn rollback_job_add(&mut self, job: &Job) {
+
+    }
+
+    pub fn add_job(&mut self, jobid: i64, dependencies: &Vec<Dependency>) {
+        if dependencies.len() == 0 {
+            return;
+        }
+
+        let mut job = Job::new(jobid);
+
+        for dependency in dependencies.iter() {
+            let result = match dependency.dep_type {
+                DependencyType::In => {
+                    self.add_in_dependency(&mut job, &dependency.label)
+                },
+                DependencyType::Out => {
+                    self.add_out_dependency(&mut job, &dependency.label);
+                    Ok(())
+                },
+                DependencyType::InOut => {
+                    let ret = self.add_in_dependency(&mut job, &dependency.label);
+                    self.add_out_dependency(&mut job, &dependency.label);
+                    ret
+                },
+            };
+            match result {
+                Ok(()) => (),
+                Err(msg) => {
+                    eprintln!("Failed to add dependency for job: {}", msg);
+                    self.rollback_job_add(&job);
+                }
+            }
+        }
+
+        self.jobs.insert(
+            jobid,
+            job
+        );
+    }
+
+    pub fn job_event(&mut self, jobid: i64, event: String) -> Result<HashSet<i64>, &'static str> {
+        let job = match self.jobs.get(&jobid) {
+            Some(x) => x,
+            None => return Err("Job ID in event not found"),
+        };
+        let mut ret = HashSet::new();
+        match event.as_str() {
+            "submit" => {
+                if job.ancestors.len() == 0 {
+                    ret.insert(jobid);
+                }
+            },
+            "finish" => {
+                for child_id in job.children.clone().iter() {
+                    let child_job = match self.jobs.get_mut(child_id) {
+                        Some(x) => x,
+                        None => return Err("Child Job ID not found"),
+                    };
+                    if !child_job.ancestors.remove(&jobid) {
+                        eprintln!("Job ID not found in child's ancestors");
+                    } else if child_job.ancestors.len() == 0 {
+                        ret.insert(child_job.jobid);
+                    }
+                }
+            },
+            _ => {},
+        }
+        Ok(ret)
     }
 }
 
@@ -32,13 +150,7 @@ impl Dependency {
     }
 }
 
-fn new_job(state: &mut State, jobid: i64, dependencies: &Vec<Dependency>) {
-    
-}
 
-fn job_event(state: &mut State, jobid: i64, event: String) -> Vec<i64> {
-    Vec::new()
-}
 
 fn main() {
     println!("Hello, world!");
@@ -48,36 +160,42 @@ fn main() {
 mod tests {
     use super::*;
 
-    fn assert_noop(vec: &Vec<i64>) {
-        assert_eq!(vec.len(), 0)
+    fn assert_noop(actual: Result<HashSet<i64>, &'static str>) {
+        assert!(actual.is_ok());
+        assert_eq!(actual.unwrap().len(), 0)
+    }
+
+    fn assert_jobs_eq(actual: Result<HashSet<i64>, &'static str>, expected: &Vec<i64>) {
+        assert!(actual.is_ok());
+        assert_eq!(actual.unwrap(), HashSet::from_iter(expected.iter().cloned()));
     }
 
     #[test]
     fn job_chain() {
         let mut state = State::new();
 
-        new_job(&mut state, 1, &vec![Dependency::new(DependencyType::Out, "foo".to_string())]);
-        new_job(&mut state, 2, &vec![Dependency::new(DependencyType::InOut, "foo".to_string())]);
-        new_job(&mut state, 3, &vec![Dependency::new(DependencyType::In, "foo".to_string())]);
+        state.add_job(1, &vec![Dependency::new(DependencyType::Out, "foo".to_string())]);
+        state.add_job(2, &vec![Dependency::new(DependencyType::InOut, "foo".to_string())]);
+        state.add_job(3, &vec![Dependency::new(DependencyType::In, "foo".to_string())]);
 
         // Submit all the things!
-        let mut out = job_event(&mut state, 1, "submit".to_string());
-        assert_eq!(out, vec![1]);
+        let out = state.job_event(1, "submit".to_string());
+        assert_jobs_eq(out, &vec![1]);
         for jobid in vec![2, 3].iter() {
-            out = job_event(&mut state, *jobid, "submit".to_string());
-            assert_noop(&out);
+            let out = state.job_event(*jobid, "submit".to_string());
+            assert_noop(out);
         }
 
         for jobid in vec![1, 2, 3].iter() {
-            out = job_event(&mut state, *jobid, "depend".to_string());
-            assert_noop(&out);
-            out = job_event(&mut state, *jobid, "alloc".to_string());
-            assert_noop(&out);
-            out = job_event(&mut state, *jobid, "finish".to_string());
+            let out = state.job_event(*jobid, "depend".to_string());
+            assert_noop(out);
+            let out = state.job_event(*jobid, "alloc".to_string());
+            assert_noop(out);
+            let out = state.job_event(*jobid, "finish".to_string());
             if *jobid < 3 {
-                assert_eq!(out, vec![jobid + 1]);
+                assert_jobs_eq(out, &vec![jobid + 1]);
             } else {
-                assert_noop(&out);
+                assert_noop(out);
             }
         }
     }
@@ -85,48 +203,59 @@ mod tests {
     #[test]
     fn job_fan_out() {
         let mut state = State::new();
-        new_job(&mut state, 1, &vec![Dependency::new(DependencyType::Out, "foo".to_string())]);
+        state.add_job(1, &vec![Dependency::new(DependencyType::Out, "foo".to_string())]);
         for jobid in vec![2, 3, 4].iter() {
-            new_job(&mut state, *jobid, &vec![Dependency::new(DependencyType::In, "foo".to_string()), Dependency::new(DependencyType::Out, "bar".to_string())]);
+            state.add_job(*jobid, &vec![Dependency::new(DependencyType::In, "foo".to_string()), Dependency::new(DependencyType::Out, "bar".to_string())]);
         }
-        new_job(&mut state, 5, &vec![Dependency::new(DependencyType::In, "bar".to_string())]);
+        state.add_job(5, &vec![Dependency::new(DependencyType::In, "bar".to_string())]);
 
         // Submit all the things!
-        let mut out = job_event(&mut state, 1, "submit".to_string());
-        assert_eq!(out, vec![1]);
+        let out = state.job_event(1, "submit".to_string());
+        assert_jobs_eq(out, &vec![1]);
         for jobid in vec![2, 3, 4, 5].iter() {
-            out = job_event(&mut state, *jobid, "submit".to_string());
-            assert_noop(&out);
+            let out = state.job_event(*jobid, "submit".to_string());
+            assert_noop(out);
         }
 
         // Run and complete initial pre-process job
-        out = job_event(&mut state, 1, "depend".to_string());
-        assert_noop(&out);
-        out = job_event(&mut state, 1, "alloc".to_string());
-        assert_noop(&out);
-        out = job_event(&mut state, 1, "finish".to_string());
-        assert_eq!(out, vec![2, 3, 4]);
+        let out = state.job_event(1, "depend".to_string());
+        assert_noop(out);
+        let out = state.job_event(1, "alloc".to_string());
+        assert_noop(out);
+        let out = state.job_event(1, "finish".to_string());
+        assert_jobs_eq(out, &vec![2, 3, 4]);
 
         // Run and complete fan-out
         for jobid in vec![2, 3, 4].iter() {
-            out = job_event(&mut state, *jobid, "depend".to_string());
-            assert_noop(&out);
-            out = job_event(&mut state, *jobid, "alloc".to_string());
-            assert_noop(&out);
-            out = job_event(&mut state, *jobid, "finish".to_string());
+            let out = state.job_event(*jobid, "depend".to_string());
+            assert_noop(out);
+            let out = state.job_event(*jobid, "alloc".to_string());
+            assert_noop(out);
+            let out = state.job_event(*jobid, "finish".to_string());
             if *jobid < 4 {
-                assert_noop(&out);
+                assert_noop(out);
             } else {
-                assert_eq!(out, vec![5]);
+                assert_jobs_eq(out, &vec![5]);
             }
         }
 
         // Run and complete postprocess job
-        out = job_event(&mut state, 5, "depend".to_string());
-        assert_noop(&out);
-        out = job_event(&mut state, 5, "alloc".to_string());
-        assert_noop(&out);
-        out = job_event(&mut state, 5, "finish".to_string());
-        assert_noop(&out);
+        let out = state.job_event(5, "depend".to_string());
+        assert_noop(out);
+        let out = state.job_event(5, "alloc".to_string());
+        assert_noop(out);
+        let out = state.job_event(5, "finish".to_string());
+        assert_noop(out);
     }
+
+    #[test]
+    fn nonexistent_in() {
+        // A job with an 'in' dependency that does not match an 'out' of a
+        // currently queued/running job can be immediately scheduled.
+        let mut state = State::new();
+        state.add_job(1, &vec![Dependency::new(DependencyType::In, "foo".to_string())]);
+        let out = state.job_event(1, "submit".to_string());
+        assert_jobs_eq(out, &vec![1]);
+    }
+
 }
